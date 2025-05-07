8b113ce99da10f5ca1bd2eac368e6ebd40d7d822
omar-sameh-mokhtar <166794956+omar-sameh-mokhtar@users.noreply.github.com>
2025-05-07 05:46:58 +0300

    Add files via upload

---
+++ routes/cartRoutes.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const cartController = require('../controllers/CartController');
+const { initializeCart} = require('../middlewares/cartMiddleware');
+
+// Apply cart middleware to all routes
+router.use(initializeCart);
+//router.use(mergeOnLogin);
+
+router.get('/', cartController.getCart);
+
+
+router.post('/add', cartController.addToCart);
+router.post('/remove/:itemId', cartController.removeItem);
+router.post('/removeAll', cartController.emptyCart);
+router.get('/count', cartController.getCount);
+router.get('/subtotal', cartController.getCartSubtotal);
+router.post('/update/:id', cartController.updateCartItemQuantity);
+//router.get('/usercart', cartController.getUserCart);
+
+
+
+module.exports = router;
